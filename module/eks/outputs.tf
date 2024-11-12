3108869dbf112062f60d3b86bda7b053a0b221e4
sangun <holic1208@daum.net>
2024-11-13 00:43:52 +0900

    generating eks module code

---
+++ module/eks/outputs.tf
@@ -0,0 +1,19 @@
+output "eks_id" {
+  value = aws_eks_cluster.eks.id
+}
+
+output "eks_endpoint" {
+  value = aws_eks_cluster.eks.endpoint
+}
+
+output "eks_ca_certificate" {
+  value = aws_eks_cluster.eks.certificate_authority.0.data
+}
+
+output "eks_oidc" {
+  value = flatten(aws_eks_cluster.eks[*].identity[*].oidc.0.issuer)[0]
+}
+
+output "eks_version" {
+  value = aws_eks_cluster.eks.version
+}
