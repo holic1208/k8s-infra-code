919f84937d82982c013e7f53666a53c4d32fc231
sangun <holic1208@daum.net>
2024-11-17 16:32:33 +0900

    change eks and node groups subnet

--- eks/main.tf
+++ eks/main.tf
@@ -3,7 +3,7 @@ module "eks" {
 
   name                    = "eks"
   eks_version             = var.eks_version
-  vpc_subnet_ids          = [data.terraform_remote_state.vpc.outputs.subnet_ids[0], data.terraform_remote_state.vpc.outputs.subnet_ids[1]]
+  vpc_subnet_ids          = [data.terraform_remote_state.vpc.outputs.subnet_ids[0], data.terraform_remote_state.vpc.outputs.subnet_ids[2]]
   vpc_security_group_ids  = [data.terraform_remote_state.sg.outputs.eks_sg_id]
   vpc_public_access_cidrs = var.vpc_public_access_cidrs
 }
