59e950f72580736c2b9e78ff87f42c719077f250
sangun <holic1208@daum.net>
2024-11-19 00:07:54 +0900

    Change external-secrets role to use pod-identity-agent

--- iam/external-secrets/main.tf
+++ iam/external-secrets/main.tf
@@ -1,8 +1,7 @@
 module "external-secrets" {
   source = "../../module/iam/external-secrets"
 
-  name      = "eks"
-  irsa_arn  = data.terraform_remote_state.irsa.outputs.irsa_arn
-  eks_oidc  = data.terraform_remote_state.eks.outputs.eks_oidc
-  sa_name   = "external-secrets"
+  name     = "eks"
+  eks_name = data.terraform_remote_state.eks.outputs.eks_id
+  sa_name  = "external-secrets"
 }
